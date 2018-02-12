22baa37fd265eb353c8f7958de5ab34a8bc65b5d
WangHao <wh1205@mail.ustc.edu.cn>
2018-02-12 17:10:02 +0800

    finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            作者:<a href="http://blog.csdn.net/hustcw98" target="_blank">Hustcw</a>
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
